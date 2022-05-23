eff695d9377e6a5bb14a507e3eabac4ff6661d9b
汉广 <serfend@foxmail.com>
2022-05-23 22:00:38 +0800

    publish[1.1.25]

--- pydumpck/__version__.py
+++ pydumpck/__version__.py
@@ -1,8 +1,8 @@
 __title__ = "pydumpck"
-__description__ = "a pyfile decomplier base on pycdc for windows."
+__description__ = "pydumpck is a tool for decompile exe,elf,pyz,pyc packed by python which is base on pycdc.sometimes its py-file result not exactly right ,maybe could use uncompyle6 etc."
 __keywords__ = ['pydumpck', 'decomplier', 'pe', 'elf', 'pyc', 'pyz']
 __url__ = "https://github.com/serfend/pydumpck"
-__version__ = "1.1.24"
+__version__ = "1.1.25"
 __author__ = "serfend"
 __author_email__ = "serfend@foxmail.com"
 __license__ = "MIT Licence"
