b60bc04ce9ed837a0a54ce601ca864acc0973574
汉广 <serfend@foxmail.com>
2022-05-24 20:49:42 +0800

    fix[entry]should use run

--- pydumpck/__init__.py
+++ pydumpck/__init__.py
@@ -1,14 +1,11 @@
 # modules here
-from pydumpck.pyinstaller_dump import CommonDump
+from pydumpck.pyinstaller_dump import run
 import sys
 import os
 src_path = os.path.realpath(__file__)
 src_dir = os.path.dirname(src_path)
 sys.path.append(src_dir)
 
-
-def run():
-    CommonDump.main()
 
 
 if __name__ == '__main__':
