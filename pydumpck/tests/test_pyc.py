3114fa813c8a4c8b3257a1783321a8a4850c77ff
汉广 <serfend@foxmail.com>
2022-05-25 16:39:40 +0800

    fix[pyc-test]should check notice content

--- pydumpck/tests/test_pyc.py
+++ pydumpck/tests/test_pyc.py
@@ -4,24 +4,23 @@ from .common import res_type
 import pydumpck.pyc_checker.lib_uncompyle6
 import pydumpck.pyc_checker
 import pydumpck.configuration
-decompiler = pydumpck.pyc_checker.lib_uncompyle6.Decompiler()
+import pydumpck.py_common_dump
 
 
 def start_pyc_test():
+    _ = pydumpck.py_common_dump.CommonDump().file_struct_pyc # start dump global
     pydumpck.configuration.decompile_file = None
     pyc_path, t = res_type.get_res('pyc')
     target_file = f'{pyc_path}.py'
     content, err = pydumpck.pyc_checker.dump_pyc(
         pyc_file=pyc_path, target_file=target_file)
     is_notice = err and err[0:3] == '[*]'
-    assert not is_notice, f'should not have notice:{err}'
-    if not is_notice:
-        assert err == None
-        assert content != None
-        assert isinstance(content, str)
-        assert len(content) > 20
-        assert os.path.exists(target_file)
-        os.remove(target_file)
+    assert err == None or is_notice
+    assert content != None
+    assert isinstance(content, str)
+    assert len(content) > 20
+    assert os.path.exists(target_file)
+    os.remove(target_file)
 
 
 def test_pyc_decompiler_pycdc():
