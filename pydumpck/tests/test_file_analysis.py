c703e544b3f14577b54054e1cf5dccbb2807b6c1
汉广 <serfend@foxmail.com>
2022-05-25 14:15:12 +0800

    doc[decompiler]test for plugins

--- pydumpck/tests/test_file_analysis.py
+++ pydumpck/tests/test_file_analysis.py
@@ -6,13 +6,29 @@ import pydumpck.configuration as configuration
 from .common.res_type import *
 import shutil
 
-def test_arch_file(res_type_arch: Tuple):
+
+def start_arch_file(res_type_arch: Tuple):
     file_path, target_type = res_type_arch
     output_dir = f'{os.path.dirname(file_path)}/output'
     configuration.thread_output_directory = output_dir
-    configuration.progress_session_timeout = 120
     dumper = dmp.CommonDump()
     dumper.handle_arch_file(file_path)
     assert current_path != None, 'none'
     assert os.path.exists(output_dir), f'fail to run'
     shutil.rmtree(output_dir)
+
+
+def test_arch_file_pycdc(res_type_arch: Tuple):
+    configuration.progress_session_timeout = 120
+    configuration.plugin_decompiler_enable_pycdc = True
+    return start_arch_file(res_type_arch)
+
+
+def test_arch_file_decompyle6(res_type_arch: Tuple):
+    configuration.progress_session_timeout = 120
+    configuration.plugin_decompiler_enable_uncompyle6 = True
+    return start_arch_file(res_type_arch)
+
+
+def test_command_line_result(res_type_all: Tuple):
+    pass
