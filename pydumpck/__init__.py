5ae7447643fd72addd94c24e4b05650692fb9876
汉广 <serfend@foxmail.com>
2022-12-11 07:36:11 +0800

    typo[tips] #15

--- pydumpck/__init__.py
+++ pydumpck/__init__.py
@@ -6,8 +6,7 @@ if bytes is str:
 pydumpck has transitioned to python 3. Due to the small size of the team behind it,
 we can't reasonably maintain compatibility between both python 2 and python 3.
 If you want to continue using the most recent version of pydumpck (you definitely
-want that, trust us) you should upgrade to python 3. It's like getting your
-vaccinations. It hurts a little bit initially but in the end it's worth it.
+want that, trust us) you should upgrade to python 3. 
 
 For more information, see here: https://github.com/serfend/pydumpck
 
---
+++ tests/test_header_custome.py
@@ -0,0 +1,10 @@
+from ..pydumpck.tests import test_file_analysis
+start_test = test_file_analysis.start_arch_file
+
+
+def get_header():
+    pass
+
+
+def test_custome_pyc_success():
+    pass
