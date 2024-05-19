767813369be3042f8bf7a8298ad2fd5a1e814d88
汉广 <serfend@foxmail.com>
2024-05-19 09:40:13 +0800

    fix[ver]ver-checker

--- pydumpck/pyc_checker/lib_uncompyle6/requirement_check.py
+++ pydumpck/pyc_checker/lib_uncompyle6/requirement_check.py
@@ -1,9 +1,14 @@
 from ... import logger
+
+
 def check(v_expected: str, v_current: str) -> bool:
     def get_ver_value(v):
         v = [int(x) for x in v.split('.')]
-        return v[0] << 24 + v[1] << 12 + v[2]
-    return get_ver_value(v_expected) <= get_ver_value(v_current)
+        return (v[0] << 24) + (v[1] << 12) + v[2]
+    cur_value = get_ver_value(v_current)
+    exp_value = get_ver_value(v_expected)
+    result = exp_value <= cur_value
+    return result, exp_value, cur_value
 
 
 def check_version():
@@ -11,7 +16,8 @@ def check_version():
     v_expected = '6.0.4'
     v_current = xdis.version.__version__
 
-    if not check(v_expected, v_current):
+    fit, exp, cur = check(v_expected, v_current)
+    if not fit:
         logger.warning(
             f'require xdis version greater than {v_expected},current is {v_current}')
         if input('install package online? yes(*)/no(n)') == 'n':
